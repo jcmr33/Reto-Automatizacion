5cc118c3db69bc5e6ef815dd89ab3dc7e669192b
jcmr33 <jcmr33@yahoo.es>
2019-10-02 19:24:18 -0500

    Actualizacion del feature y de dos tasks

--- src/main/java/pe/metro/www/task/ClicMinicart.java
+++ src/main/java/pe/metro/www/task/ClicMinicart.java
@@ -1,9 +1,12 @@
 package pe.metro.www.task;
 
 import static net.serenitybdd.screenplay.Tasks.instrumented;
+import static net.serenitybdd.screenplay.matchers.WebElementStateMatchers.isPresent;
+
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.actions.Click;
+import net.serenitybdd.screenplay.waits.WaitUntil;
 import pe.metro.www.userinterface.MetroHomePage;
 
 /**
@@ -17,7 +20,8 @@ public class ClicMinicart implements Task{
 	 @Override
 	    public <T extends Actor> void performAs(T actor) {
 		 actor.attemptsTo(
-	                Click.on(MetroHomePage.BTN_MINICART)
+	                Click.on(MetroHomePage.BTN_MINICART),
+	                WaitUntil.the(MetroHomePage.LABEL_NAME_CART, isPresent())
 	        );
 	    }
  
